e3ef100a2666d3f689b4a8ea23e01b3ba593d100
Pawan Kaushal <pawancse1234@gmail.com>
2019-01-06 10:03:38 +0530

    added initial commit - templates

---
+++ custom-starter-consumer-property/src/main/java/org/custom/starter/consumer/property/App.java
@@ -0,0 +1,13 @@
+package org.custom.starter.consumer.property;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ custom-starter-mongo/src/main/java/com/example/mongo/CustomStarterMongoApplication.java
@@ -0,0 +1,14 @@
+package com.example.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomStarterMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomStarterMongoApplication.class, args);
+	}
+
+}
+
